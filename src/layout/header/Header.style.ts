b1d72fae3050994d58a98bc44b177c95de91ccaf
Sukhveer Sangha <sukhveers@push.live>
2023-07-26 15:29:56 +0100

    added header styled component

--- src/layout/header/Header.style.ts
+++ src/layout/header/Header.style.ts
@@ -1,26 +1,49 @@
 import styled from "styled-components";
 
-const Container = styled.nav`
-  ul {
+const Container = styled.header`
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+
+  .btn:link,
+  .btn:visited {
+    display: inline-block;
+    padding: 10px 10px;
+    font-weight: 600;
     text-decoration: none;
-    list-style: none;
-    display: flex;
-    height: 5rem;
-    width: auto;
-    background-color: green;
-    margin: 0;
-    color: #fff;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: flex-end;
-    align-items: center;
-    gap: 2rem;
-    padding-right: 2rem;
+    border-radius: 200px;
+    -webkit-transition: background-color 0.2s, color 0.2s;
+    transition: background-color 0.2s, color 0.2s;
   }
 
-  li {
-    font-size: 1rem;
-    cursor: pointer;
+  .btn-ghost:link,
+  .btn-ghost:visited {
+    border: 4px solid #6f1e51;
+    color: #6f1e51;
+    background-color: rgba(255, 255, 255, 0.6);
+  }
+
+  .btn:hover,
+  .btn:active {
+    background-color: rgba(71, 19, 52, 0.6);
+    color: #fff;
+  }
+
+  .web-dev {
+    font-style: italic;
+    text-transform: none;
+    font-size: 200%;
+    text-shadow: 2px 2px #000;
+  }
+
+  .hero-text-box {
+    position: absolute;
+    width: 1140px;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    z-index: 1;
   }
 `;
 
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,14 +1,26 @@
 import React from "react";
+import Navigation from "../navigation/Navigation";
 import Container from "./Header.style";
 
 const Header: React.FC = () => {
   return (
     <Container>
-      <ul>
-        <li>Projects</li>
-        <li>Work Experience</li>
-        <li>Contact</li>
-      </ul>
+      <div className="hero-text-box">
+        <h1>Sukh Sangha</h1>
+        <h1 className="web-dev">Web Developer</h1>
+        <a className="btn btn-ghost js--scroll-to-projects" href="#">
+          View Projects
+        </a>
+      </div>
+      <div className="crossfade">
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+      </div>
+      <Navigation />
     </Container>
   );
 };
