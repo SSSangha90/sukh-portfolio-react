2991311164cf31c2031a802da3edcab0727be8a2
Sukhveer Sangha <sukhi_sangha_11@hotmail.com>
2023-07-31 15:44:07 +0100

    link to budget app, new budget app folder, route added to component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import AppContainer from "./App.styled";
 import LandingPage from "./pages/Landing";
+import BudgetApp from "./pages/projects/budgetapp/BudgetApp";
 import ProjectsPage from "./pages/projects/ProjectsPage";
 
 const HomePage: React.FC = () => {
@@ -11,6 +12,7 @@ const HomePage: React.FC = () => {
       <Routes>
         <Route path="/*" element={<LandingPage />} />
         <Route path="/projects/*" element={<ProjectsPage />} />
+        <Route path="/projects/budgetapp" element={<BudgetApp />} />
       </Routes>
     </AppContainer>
   );
---
+++ src/pages/projects/budgetapp/BudgetApp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BudgetApp: React.FC = () => {
+  return <h1>Budget App</h1>;
+};
+
+export default BudgetApp;
