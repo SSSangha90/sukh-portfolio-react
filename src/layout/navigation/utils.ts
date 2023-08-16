da6223d311ddf7cb9d307b007c54f4db8b57f8c4
Sukhveer Sangha <sukhi_sangha_11@hotmail.com>
2023-08-16 17:37:52 +0100

    route to home page, conditionally render nav links based on location

---
+++ src/layout/navigation/utils.ts
@@ -0,0 +1,23 @@
+interface ILinks {
+  title: string;
+  url: string;
+}
+
+export const links: ILinks[] = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About Me",
+    url: "/about",
+  },
+  {
+    title: "Projects",
+    url: "/projects",
+  },
+  {
+    title: "Experience",
+    url: "/experience",
+  },
+];
